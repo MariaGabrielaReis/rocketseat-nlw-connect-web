03675be797195787835612bbe655e341f1cf98c8
MariaGabrielaReis <mariagabrielagreis@gmail.com>
2025-02-22 17:48:47 -0300

    feat: integrate ranking

--- src/app/invite/[subscriberId]/ranking.tsx
+++ src/app/invite/[subscriberId]/ranking.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 
+import { getRanking } from "@/http/api";
 import medalCooper from "../../../assets/medal-cooper.svg";
 import medalGold from "../../../assets/medal-gold.svg";
 import medalSilver from "../../../assets/medal-silver.svg";
 
-export function Ranking() {
+export async function Ranking() {
+  const { ranking } = await getRanking();
+
   return (
     <div className="w-full max-w-[440px] space-y-5">
       <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
@@ -12,48 +15,46 @@ export function Ranking() {
       </h2>
 
       <div className="space-y-4">
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">1º</span> | Maria Gabriela Reis
-          </span>
-          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-            1030
-          </span>
+        {ranking.map((item, index) => {
+          const rankingPosition = index + 1;
 
-          <Image
-            src={medalGold}
-            alt="Medalha de ouro"
-            className="absolute top-0 right-8"
-          />
-        </div>
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">2º</span> | Maria Gabriela Reis
-          </span>
-          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-            1030
-          </span>
+          return (
+            <div
+              key={item.id}
+              className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3"
+            >
+              <span className="text-sm text-gray-300 leading-none">
+                <span className="font-semibold">{rankingPosition}º</span> |{" "}
+                {item.name}
+              </span>
+              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+                {item.score}
+              </span>
 
-          <Image
-            src={medalSilver}
-            alt="Medalha de prata"
-            className="absolute top-0 right-8"
-          />
-        </div>
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">3º</span> | Maria Gabriela Reis
-          </span>
-          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-            1030
-          </span>
-
-          <Image
-            src={medalCooper}
-            alt="Medalha de cobre"
-            className="absolute top-0 right-8"
-          />
-        </div>
+              {rankingPosition === 1 && (
+                <Image
+                  src={medalGold}
+                  alt="Medalha de ouro"
+                  className="absolute top-0 right-8"
+                />
+              )}
+              {rankingPosition === 2 && (
+                <Image
+                  src={medalSilver}
+                  alt="Medalha de prata"
+                  className="absolute top-0 right-8"
+                />
+              )}
+              {rankingPosition === 3 && (
+                <Image
+                  src={medalCooper}
+                  alt="Medalha de cobre"
+                  className="absolute top-0 right-8"
+                />
+              )}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
